3a8754d0213dc1be09894560310ddea5d25fc668
ajaxswan <ajaxswan.tbs@gmail.com>
2024-06-20 20:02:59 +0700

    html5 construct test

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1718877536,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/chicken-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/duck-sheet0.png",
+		"images/inventoryframe-sheet0.png",
+		"images/sloty.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
